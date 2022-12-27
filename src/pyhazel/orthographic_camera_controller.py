6b8098c15910aebe5f719bcd9c7d3d3a771d0157
Thomas <11610634+twje@users.noreply.github.com>
2022-12-27 17:34:13 +1100

    Fixes for OrthographicCameraController Movement

--- src/pyhazel/orthographic_camera_controller.py
+++ src/pyhazel/orthographic_camera_controller.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
+import math
 from pyhazel.renderer import OrthographicCamera
 from pyhazel.input import Input
 from pyhazel.key_codes import *
@@ -38,14 +39,17 @@ class OrthographicCameraController:
 
     def update(self, ts: Timestep):
         if Input.is_key_pressed(HZ_KEY_A):
-            self.camera_position.x -= self.camera_translation_speed * ts.seconds
+            self.camera_position.x -= self.calc_anti_cos_rotate(ts)
+            self.camera_position.y -= self.calc_anti_sin_rotate(ts)
         elif Input.is_key_pressed(HZ_KEY_D):
-            self.camera_position.x += self.camera_translation_speed * ts.seconds
-
+            self.camera_position.x += self.calc_anti_cos_rotate(ts)
+            self.camera_position.y += self.calc_anti_sin_rotate(ts)
         if Input.is_key_pressed(HZ_KEY_W):
-            self.camera_position.y += self.camera_translation_speed * ts.seconds
+            self.camera_position.x += -self.calc_anti_sin_rotate(ts)
+            self.camera_position.y += self.calc_anti_cos_rotate(ts)
         elif Input.is_key_pressed(HZ_KEY_S):
-            self.camera_position.y -= self.camera_translation_speed * ts.seconds
+            self.camera_position.x -= -self.calc_anti_sin_rotate(ts)
+            self.camera_position.y -= self.calc_anti_cos_rotate(ts)
 
         if self.is_rotation_enabled:
             if Input.is_key_pressed(HZ_KEY_Q):
@@ -53,11 +57,22 @@ class OrthographicCameraController:
             elif Input.is_key_pressed(HZ_KEY_E):
                 self.camera_rotation -= self.camera_rotation_speed * ts.seconds
 
+            if self.camera_position > 180:
+                self.camera_rotation -= 360
+            elif self.camera_rotation <= -180:
+                self.camera_rotation += 360
+
             self.camera.rotation = self.camera_rotation
 
         self.camera.position = self.camera_position
 
         self.camera_translation_speed = self.zoom_level
+
+    def calc_anti_cos_rotate(self, ts: Timestep):
+        return math.cos(glm.radians(self.camera_rotation)) * self.camera_translation_speed * ts.seconds
+
+    def calc_anti_sin_rotate(self, ts: Timestep):
+        return math.sin(glm.radians(self.camera_rotation)) * self.camera_translation_speed * ts.seconds
 
     def on_event(self, event: Event):
         dispatcher = EventDispatcher(event)
