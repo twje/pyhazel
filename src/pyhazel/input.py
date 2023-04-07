18de44518874594983d147a2d67c0ea4a6e77ab9
Thomas <11610634+twje@users.noreply.github.com>
2023-04-07 13:47:19 +1000

    Refactored Input situation

--- src/pyhazel/input.py
+++ src/pyhazel/input.py
@@ -10,61 +10,30 @@ __all__ = ["Input"]
 
 
 class Input(ABC):
-    instance: "Input" = None
-
     # ----------
     # Client API
     # ----------
     @staticmethod
-    def create() -> Input:
-        if PLATFORM == Platform.HZ_PLATFORM_WINDOWS:
-            from .windows_input import WindowsInput
-            return WindowsInput()
-        else:
-            assert False, "Unknown platform!"
-
-    @classmethod
-    def is_key_pressed(cls, keycode: KeyCode) -> bool:
-        return cls.instance.is_key_pressed_impl(keycode)
-
-    @classmethod
-    def is_mouse_button_pressed(cls, button: MouseCode) -> bool:
-        return cls.instance.is_mouse_button_pressed_impl(button)
-
-    @classmethod
-    def get_mouse_position(cls) -> tuple[int, int]:
-        return cls.instance.get_mouse_position_impl()
-
-    @classmethod
-    def get_mouse_x(cls) -> int:
-        return cls.instance.get_mouse_x_impl()
-
-    @classmethod
-    def get_mouse_y(cls) -> int:
-        return cls.instance.get_mouse_y_impl()
-
-    # ----------------
-    # Abstract Methods
-    # ----------------
     @abstractmethod
-    def is_key_pressed_impl(self, keycode: KeyCode) -> bool:
+    def is_key_pressed(keycode: KeyCode) -> bool:
         pass
 
+    @staticmethod
     @abstractmethod
-    def is_mouse_button_pressed_impl(self, button: MouseCode) -> bool:
+    def is_mouse_button_pressed(button: MouseCode) -> bool:
         pass
 
+    @staticmethod
     @abstractmethod
-    def get_mouse_position_impl(self) -> tuple[int, int]:
+    def get_mouse_position() -> tuple[int, int]:
         pass
 
+    @staticmethod
     @abstractmethod
-    def get_mouse_x_impl(self) -> int:
+    def get_mouse_x() -> int:
         pass
 
+    @staticmethod
     @abstractmethod
-    def get_mouse_y_impl(self) -> int:
+    def get_mouse_y() -> int:
         pass
-
-
-Input.instance = Input.create()
--- src/pyhazel/windows_input.py
+++ src/pyhazel/windows_input.py
@@ -8,24 +8,24 @@ __all__ = ["WindowsInput"]
 
 
 class WindowsInput(Input):
-    def is_key_pressed_impl(self, keycode: KeyCode) -> bool:
+    def is_key_pressed(keycode: KeyCode) -> bool:
         window = Application.instance.window.native_window
         state = glfw.get_key(window, int(keycode))
         return state == glfw.PRESS or state == glfw.REPEAT
 
-    def is_mouse_button_pressed_impl(self, button: MouseCode) -> bool:
+    def is_mouse_button_pressed(button: MouseCode) -> bool:
         window = Application.instance.window.native_window
         state = glfw.get_mouse_button(window, int(button))
         return state == glfw.PRESS
 
-    def get_mouse_position_impl(self) -> tuple[int, int]:
+    def get_mouse_position() -> tuple[int, int]:
         window = Application.instance.window.native_window
         return glfw.get_cursor_pos(window)
 
-    def get_mouse_x_impl(self) -> int:
-        x, _ = self.get_mouse_position_impl()
+    def get_mouse_x() -> int:
+        x, _ = WindowsInput.get_mouse_position()
         return x
 
-    def get_mouse_y_impl(self) -> int:
-        _, y = self.get_mouse_position_impl()
+    def get_mouse_y() -> int:
+        _, y = WindowsInput.get_mouse_position()
         return y
